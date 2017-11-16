3737fe99df45714d410bedb7109c6e78a4e974be
Michael Kaaden <m.kaaden@mid.de>
2017-11-16 11:55:32 +0100

    Set version to 0.100.3

--- dist/js/materialize.min.js
+++ dist/js/materialize.min.js
@@ -1,5 +1,5 @@
 /*!
- * Materialize vundefined (http://materializecss.com)
+ * Materialize v0.100.3 (http://materializecss.com)
  * Copyright 2014-2017 Materialize
  * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
  */
